ed8fa8efd8c983a94854da05f0ded52c08990970
yoshikouki <yoshikouki@gmail.com>
2022-02-24 23:51:41 +0900

    エラーの切り分けのために /health を用意

--- next.config.js
+++ next.config.js
@@ -3,7 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     host: "financial-chart-18a4c.uc.r.appspot.com"
-  }
+  },
+  rewrites: async () => {
+    return [
+      {
+        source: '/health',
+        destination: '/api/health',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
