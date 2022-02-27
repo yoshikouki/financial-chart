8cbe8b302ffabaf0f05803fc055ca86a5304ca7f
yoshikouki <yoshikouki@gmail.com>
2022-02-27 13:23:39 +0900

    ヘッダーの位置固定をシンプルにしてIE11は切り捨てた

--- src/components/TopNavigationBar.tsx
+++ src/components/TopNavigationBar.tsx
@@ -1,42 +1,21 @@
-import { AppBar, Toolbar, Typography, useScrollTrigger } from "@mui/material";
-import React from "react";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import { DefaultLayoutProps } from "./layouts/default";
-
-interface ElevationScrollProps {
-  window?: () => Window;
-  children: React.ReactElement;
-}
-
-const ElevationScroll = (props: ElevationScrollProps) => {
-  const { children, window } = props;
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 0,
-    target: window ? window() : undefined,
-  });
-
-  return React.cloneElement(children, {
-    elevation: trigger ? 4 : 0,
-  });
-};
 
 const TopNavigationBar = (props: DefaultLayoutProps) => {
   return (
-    <ElevationScroll {...props}>
-      <AppBar>
-        <Toolbar>
-          <Typography
-            component="h1"
-            variant="h6"
-            color="inherit"
-            noWrap
-            sx={{ flexGrow: 1 }}
-          >
-            {props.title || "Financial Chart"}
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <AppBar position="sticky">
+      <Toolbar>
+        <Typography
+          component="h1"
+          variant="h6"
+          color="inherit"
+          noWrap
+          sx={{ flexGrow: 1 }}
+        >
+          {props.title || "Financial Chart"}
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 };
 
